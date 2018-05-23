52096979a6a4a121e08fb628cc6296b44990d5cb
HoGyeong Leo Cho <chg8778@gmail.com>
2018-05-23 12:55:02 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+var cols, rows;
+var scl = 25;
+var w = 800;
+var h = 600;
+
+var flying = 0;
+
+var terrain = [];
+
+function setup() {
+  createCanvas(400, 400, WEBGL);
+  cols = w / scl;
+  rows = h/ scl;
+
+  for (var x = 0; x < cols; x++) {
+    terrain[x] = [];
+    for (var y = 0; y < rows; y++) {
+      terrain[x][y] = 0; //specify a default value for now
+    }
+  }
+}
+
+function draw() {
+
+  flying -= 0.1;
+  var yoff = flying;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
+      xoff += 0.2;
+    }
+    yoff += 0.2;
+  }
+
+  background(0);
+
+  //background('rgba(0,0,0, 0.25)');
+  translate(0, 50);
+  rotateX(PI/3);
+  //nofill();
+  stroke(100, 100, 240);
+  fill('rgba(0,0,0,0.25)');
+  translate(-w/2, -h/2);
+  for (var y = 0; y < rows-1; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (var x = 0; x < cols; x++) {
+      vertex(x*scl, y*scl, terrain[x][y]);
+      vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
+    }
+    endShape();
+  }
+}
